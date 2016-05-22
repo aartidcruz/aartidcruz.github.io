ceaae9030adb5b942df496ee010dd1b7c352510c
Aarti D'Cruz <aarti@creativelicence.com.au>
2016-05-22 14:44:55 +1000

    remove some stuff

--- js/autoprefix.js
+++
@@ -1,8 +0,0 @@
-var stylus = require('stylus');
-var autoprefixer = require('autoprefixer-stylus');
-
-stylus(css)
-  .use(autoprefixer())
-  .render(function(err, output){
-    console.log(output);
-  });
